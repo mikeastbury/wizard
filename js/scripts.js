b22f3fa4e45e042a19e8c7d78b1571bc475b5e5b
mike.astbury <mikeastbury@gmail.com>
2018-11-16 16:14:55 -0500

    JS edits
    Adding in the beginning of multiple players, arrays, objects, FUN

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,51 @@
 let STATE = {
-    playerNames: [],
-    playerNumber: [],
-    numberOfPlayers: 0,
+    playerDetails: [],
+    numberOfPlayers: 1,
     currentRound: 0,
     maxRounds: 0
 }
 
 function firstLoad() {
-    var i = 1;
-    for (i = 1; i < 4; i++ ) {
-        
+    var i = 0;
+    for (i = 0; i < 3; i++) {
         drawPlayer();
+        createPlayerArray();
     }
 }
 
+function createPlayerArray() {
+    STATE.numberOfPlayers++;
+    const players = STATE.playerDetails;
+    const playerNumber = STATE.numberOfPlayers;
+    players.push(playerNumber);
+    console.log(players);
+}
+
 function addPlayer() {
-    $('.addPlayer').click(function () {
-        STATE.numberOfPlayers++;
+    $('.addPlayer').click(function() {
         drawPlayer();
+        createPlayerArray();
+        //remove add player button at max
+        if (STATE.numberOfPlayers > 6) {
+            $('.addPlayer').addClass('hidden');
+        }
     });
 };
 
-function drawPlayer() {
+// function addName() {
+//     $('.addName').click(function() {
+//         let name = "Mike";
+//         let players = STATE.playerDetails;
+//     })
+// }
+
+function drawPlayer(createPlayerArray) {
+    const playerNumber = STATE.numberOfPlayers;
     $('.playersAndNames').append(
         `
             <div class="columns">
                 <div class="column">
-                    <p>Player 1</p>
+                    <p>Player ${playerNumber}</p>
                 </div>
                 <div class="column">
                     <input class="input" type="text" placeholder="Name">
@@ -40,4 +59,6 @@ function drawPlayer() {
 
 $(document).ready(function () {
     firstLoad();
+    addPlayer();
+    createPlayerArray();
 });
